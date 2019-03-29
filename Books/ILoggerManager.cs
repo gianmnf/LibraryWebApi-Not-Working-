52307b175e514579a9c9d8add60dc267ab508812
Gian Michel <gianveloxsi@gmail.com>
2019-03-29 20:22:44 -0300

    Implementando NLog

---
+++ Books/ILoggerManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books
+{
+    public interface ILoggerManager
+    {
+        //Criando mensagens de log
+        void LogInfo(string mensagem);
+        void LogWarn(string mensagem);
+        void LogDebug(string mensagem);
+        void LogError(string mensagem);
+    }
+}
