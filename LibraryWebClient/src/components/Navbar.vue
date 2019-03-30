3c96dc1d8221861ae9dac7a43ad74634660e7107
Gian Michel <gianveloxsi@gmail.com>
2019-03-30 14:51:37 -0300

    Navbar criada

---
+++ LibraryWebClient/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <b-navbar
+    toggleable="md"
+    type="dark"
+    variant="info">
+    <b-navbar-brand href="#">Library Web Home</b-navbar-brand>
+    <b-navbar-nav>
+        <b-nav-item href="#">Library Actions</b-nav-item> 
+        <b-nav-item href="#">Library Actions</b-nav-item> 
+    </b-navbar-nav>
+    </b-navbar>
+</template>
+<script>
+export default {
+    name: 'Navbar'
+};
+</script>
