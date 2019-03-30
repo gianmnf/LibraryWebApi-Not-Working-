da2400ecad17993c70dd0a640b6223236b3079e0
Gian Michel <gianveloxsi@gmail.com>
2019-03-30 11:53:30 -0300

    Alterando Query

--- query.sql
+++ query.sql
@@ -1,11 +1,11 @@
 create schema livraria
 use livraria
 create table if not exists livros(
-	livroId int not null auto_increment,
+    livroId char(36) not null,
     nome varchar(150) not null,
     preco float not null,
     tipo varchar(80) not null,
     autor varchar(100) not null,
     primary key (livroId)
     ) ENGINE=INNODB;
-    insert into livros(nome,preco,tipo,autor) values ('Harry Potter e a Pedra Filosofal',29.90,'Aventura','J.K Rowlling')
+    insert into livros(livroId,nome,preco,tipo,autor) values ('d4599e236d5d41a694edc6bbc4d13a57','Harry Potter e a Pedra Filosofal',29.90,'Aventura','J.K Howlling')
