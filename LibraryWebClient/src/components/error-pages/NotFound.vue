e2a8d7c95d5c3d996c34b9f38445e9111d6193c9
Gian Michel <gianveloxsi@gmail.com>
2019-03-30 15:16:21 -0300

    Adicionando página de erro 404

---
+++ LibraryWebClient/src/components/error-pages/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>
+    Erro 404 - Página não encontrada!
+  </p>
+</template>
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+<style scoped>
+p {
+    font-weight: bold;
+    font-size: 50px;
+    text-align: center;
+    color: #f10b0b;
+}
+</style>
